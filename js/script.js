80618c02cac4c756e81f8eaa799fdb88f3f7d1a2
Roys wanimbo <157364709+royswanimbo@users.noreply.github.com>
2025-05-23 02:14:23 +0900

    Deskripsi perubahan

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function updateModal(title, content) {
+  document.getElementById("infoModalLabel").innerText = title;
+  document.getElementById("modalContent").innerText = content;
+}
+
+// Tampilkan file yang diupload dalam bentuk link
+// let preview = "";
+// if (kk) preview += `<p>KK: <a href="#" target="_blank">${kk}</a></p>`;
+// if (rapor) preview += `<p>Rapor: <a href="#" target="_blank">${rapor}</a></p>`;
+// if (akta) preview += `<p>Akta: <a href="#" target="_blank">${akta}</a></p>`;
+// localStorage.setItem("previewFiles", preview);
